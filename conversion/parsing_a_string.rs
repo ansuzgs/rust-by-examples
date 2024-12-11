7c3f2a56bfa092e3620004a19d1487601100a93d
Pablo Gonzalez Santatecla <paos@gmv.es>
2024-12-11 12:27:28 +0100

    covnersion

---
+++ conversion/parsing_a_string.rs
@@ -0,0 +1,29 @@
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct Circle {
+    radius: i32,
+}
+
+impl FromStr for Circle {
+    type Err = ParseIntError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.trim().parse() {
+            Ok(num) => Ok(Circle{ radius: num }),
+            Err(e) => Err(e),
+        }
+    }
+}
+
+fn main() {
+    let parsed: i32 = "5".parse().unwrap();
+    let turbo_parsed = "10".parse::<i32>().unwrap();
+
+    let sum = parsed + turbo_parsed;
+    println!("Sum: {:?}", sum);
+
+    let radius = "    3 ";
+    let circle: Circle = radius.parse().unwrap();
+    println!("{:?}", circle);
+}
