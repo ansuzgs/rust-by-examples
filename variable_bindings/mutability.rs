73f3e0b235747050650f5bc4b6c525fa4541a586
Pablo Gonzalez Santatecla <paos@gmv.es>
2024-12-11 10:36:53 +0100

    variable_bindings

---
+++ variable_bindings/mutability.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let _inmutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+
+    // Ok
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+}
