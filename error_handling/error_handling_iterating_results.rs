d4494c5749c235405ecde0060376d772d1a3064e
Pablo Gonzalez Santatecla <paos@gmv.es>
2024-12-15 18:31:47 +0100

    Finishing error handling...

---
+++ error_handling/error_handling_iterating_results.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Vec<_> = strings.into_iter().map(|s| s.parse::<i32>()).collect();
+    println!("Results: {:?}", numbers);
+
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Vec<_> = strings.into_iter().filter_map(|s| s.parse::<i32>().ok()).collect();
+    println!("Results: {:?}", numbers);
+}
